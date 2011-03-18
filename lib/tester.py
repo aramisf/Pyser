3f678f5a23e4c27db52238326c8cc451b4a6dd46
aramis <aramis@tramp.(none)>
2011-03-18 11:51:01 -0300

    Creating structures containin DXF file descriptions

--- lib/tester.py
+++ lib/tester.py
@@ -3,9 +3,9 @@
 # This program is just a simple interface. The main program should use the
 # libs in this directory.
 
-from DXFDefinitions import TEXT 
+from DXFDefinitions import *
 
+# It is not working yet, but this should be called approximately like this:
 f = open("../examples/ex1.dxf")
-print 'abri'
-TEXT(f)
-print 'abri'
+
+TABLES(f)
