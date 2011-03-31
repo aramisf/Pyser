6d3fa42e31d516e409dc6af903edbc84a70deee7
aramis <aramis@tramp.(none)>
2011-03-31 11:29:54 -0300

    Corrected path variable.
    Now the program can be called from any directory.

--- bin/DXF2TXT.py
+++ bin/DXF2TXT.py
@@ -2,7 +2,10 @@
 
 from PyQt4 import QtCore, QtGui
 from sys import argv, path, exit as sys_exit
-path.append('../lib/')
+from os.path import dirname, abspath
+
+# Updating path to import DXF lib:
+path.append(dirname(abspath(argv[0]))+"/../lib/")
 
 # Local modules:
 from Interface import *
